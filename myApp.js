8f13d1dfc37728967efea5c9ce843bd690658541
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2024-10-03 17:22:41 +0000

    edit myApp.js edit package.json

--- myApp.js
+++ myApp.js
@@ -1,39 +1,23 @@
 let express = require('express');
+require('dotenv').config();
+const res = require('express/lib/response');
 let app = express();
 
+var message;
 
+app.use('/public', express.static(__dirname+'/public'))
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname+"/views/index.html");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = "Hello json".toUpperCase();
+      } else {
+        message = "Hello json";
+      }
+        res.json({"message":message});
+});
 
  module.exports = app;
