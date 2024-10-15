0948c751d528e1140a7eb8061ca6ddd742bb9164
Gabriele Grandinetti <gabriele.grandinetti@hotmail.com>
2024-10-15 20:03:32 +0000

    commit

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+var bodyParser = require('body-parser');
 let express = require('express');
 require('dotenv').config();
 const res = require('express/lib/response');
@@ -6,6 +7,8 @@ let app = express();
 var message;
 
 app.use('/public', express.static(__dirname+'/public'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
 app.use(function middleware(req,res,next){
     console.log(req.method +" "+ req.path + " - " + req.ip)
@@ -24,5 +27,41 @@ app.get("/json", (req, res) => {
       }
         res.json({"message":message});
 });
+
+app.get("/:word/echo", (req,res)=>{
+  const word =  req.params.word;
+  res.json({
+    "echo":word
+  });
+})
+
+/* app.get("/name" ,(req,res)=>{
+  var  {frist = frist, last= last}=req.query;
+  res.json({
+    "name" : frist + " " + last
+  });
+}) */
+
+app.get("/name", function(req, res) {
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
  module.exports = app;
